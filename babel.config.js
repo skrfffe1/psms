2058fb13e91ca7da2a42a8fec42835290c43d6d4
Angelito Domingo <ezkyuhiro24@gmail.com>
2025-05-08 12:10:37 +0800

    deleted files and configure babel

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
-export default function (api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      env: {
-        production: {
-          plugins: ['react-native-paper/babel'],
-        },
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
       },
-    };
+    },
   };
+};
